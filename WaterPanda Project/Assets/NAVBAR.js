390d0a7ea0bf01039dc9699ba9f804cdbe725a33
Muhammad Khateeb Ejaz <160773008+MuhammadKhateebEjaz@users.noreply.github.com>
2025-06-15 15:44:39 +0500

    Add files via upload

---
+++ WaterPanda Project/Assets/NAVBAR.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const menuToggle = document.getElementById("menuToggle");
+  // Ensure the menu is closed on page load
+  document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.remove("active");
+  });
