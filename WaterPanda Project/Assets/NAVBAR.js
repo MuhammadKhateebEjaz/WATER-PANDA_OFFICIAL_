f9bb50213cbcb0d41f57c8ec25b55e210d404481
Muhammad Khateeb Ejaz <160773008+MuhammadKhateebEjaz@users.noreply.github.com>
2025-06-15 16:22:42 +0500

    Delete WaterPanda Project/Assets directory

--- WaterPanda Project/Assets/NAVBAR.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const menuToggle = document.getElementById("menuToggle");
-  // Ensure the menu is closed on page load
-  document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.getElementById("navLinks");
-    navLinks.classList.remove("active");
-  });
